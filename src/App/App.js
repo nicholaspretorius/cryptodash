3444d9755700068d784250ed27efbc51edf47375
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-01-19 09:49:35 +0200

    Added propTypes.

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./App.css";
+
+const Welcome = props => {
+  return <h1>Hello {props.name}</h1>;
+};
 
 class App extends Component {
   render() {
-    return (
-     <div>Hello Cryptodash!</div>
-    );
+    return <Welcome name="CryptoDash" />;
   }
 }
+
+Welcome.propTypes = {
+  name: PropTypes.string.isRequired
+};
 
 export default App;
